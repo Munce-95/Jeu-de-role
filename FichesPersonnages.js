6b0d565d374200022dc6f951dae94cc6295587b1
Munce-95 <celian.gremy@gmail.com>
2025-02-25 13:28:23 +0100

    Update Key

--- FichesPersonnages.js
+++ FichesPersonnages.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://sxwltroedzxkvqpbcqjc.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4d2x0cm9lZHp4a3ZxcGJjcWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MjQxNzIsImV4cCI6MjA1NjAwMDE3Mn0.F_XIxMSvejY2xLde_LbLcLt564fiW2zF-wqr95rZ2zA";
 const API_URL = `${SUPABASE_URL}/rest/v1/personnages`;
 
 async function chargerPersonnages() {
